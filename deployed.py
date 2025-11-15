7b9e4c38bb914ec427422de47a2b16b19e5f8a81
Sakshamjha27 <sakshamjha279@gmail.com>
2025-11-15 21:43:39 +0530

    Updated Hotel Cancellation Case study

--- deployed.py
+++ deployed.py
@@ -1,53 +1,57 @@
-import numpy as np 
+import numpy as np
 import pandas as pd
-import streamlit as st 
-import joblib 
+import streamlit as st
+import joblib
 
-#lets load all the instances req over here 
+# Lets load all the instances required over here
+with open('transformer.joblib','rb') as file:
+    transformer = joblib.load(file)
 
-with open('transformer.joblib','rb') as file :
-    transformer = joblib.load(file) 
-    
-#lets load the model 
+with open('final_model.joblib','rb') as file:
+    model = joblib.load(file)
 
-with open('final_model.joblib','rb') as file :
-    model = joblib.load(file) 
-    
-st.title('Inn Hotel Group')
-st.header('Thi application wll predicted the chances of booking cancellelations')
+st.title('INN HOTEL GROUP')
+st.header(':blue[This application will predict the chances of booking cancellation]')
 
-#lets take input from the user 
-amnth = st.slider('Select your month r arrival',min_vlaue = 1 , max_value= 12)
-wkd_lambda = (lambda x:0 if x=='Mon' else 
-              1 if x=='Tue' else 
-              2 if x=='Weds' else 
-              3 if x=='Thrus' else 
-              4 if x=='Fri' else 
-              5 if x=='Sat' else 
-              6) 
-awkd = st.selectbox('select your weekday of arrival',['Mon','Tues','Weds','Thrus','Fri','Sat','Sun']) 
-dwkd = st.selectbox('select your weekday of departure',['Mon','Tues','Weds','Thrus','Fri','Sat','Sun']) 
-wkend = st.number_input('Enter How many weekend nights are there in stay',min_values=0)
-wk = st.number_input('Enter How many weekend nights are there in stay',min_values=0)
-totn = wkend + wk 
-mkt = (lambda x: 0 if x=='Ofline' else 1 )(st.selectbox('How is booking took place?',['Offline','Online']))
-lt = st.number_input('how many days prior booking was done?',min_value = 0 )
-price = st.number_input('What i tha avg price?',min_value=0) 
-adults = st.number_input('how many adult members in booking',min_value = 1)
-spcl = st.selectbox('How maNy select request maade ?',[0,1,2,3,4,5])
-park = (lambda x:0 if x=='No' else 1)(st.selectbox('How guest need parking spaces',['Yes','No']))
+# Lets take input from users
+amnth = st.slider('Select our month of arrival',min_value=1,max_value=12,step=1)
+wkd_lambda = (lambda x:0 if x=='Mon' else 1 if x=='Tue' else
+                 2 if x=='Wed' else 3 if x=='Thu' else
+                 4 if x=='Fri' else 5 if x=='Sat' else 6)
+awkd = wkd_lambda(st.selectbox('Select your weekday of arrival',['Mon','Tue','Wed','Thu','Fri','Sat','Sun']))
 
-#Transform the data
-lt_t,price_t = transformer.tranform([[lt,price]])[0]
+dwkd = wkd_lambda(st.selectbox('Select your weekday of departure',['Mon','Tue','Wed','Thu','Fri','Sat','Sun']))
+
+wkend = st.number_input('Enter how many weekend nights you will stay',min_value=0)
+
+wk = st.number_input('Enter how many week nights you will stay',min_value=0)
+
+totn = wkend + wk
+
+mrkt = (lambda x:0 if x=='Offline' else 1)(st.selectbox('How booking has been made',['Offline','Online']))
+
+lt = st.number_input('How many days prior booking was made?',min_value=0)
+
+price = st.number_input('How many days prior price per room?',min_value=0)
+
+adults = st.number_input('How many adults members in your booking?',min_value=0)
+
+spcl = st.selectbox('Select the specail request you made',[0,1,2,3,4,5])
+park = (lambda x:0 if x=='No' else 1)(st.selectbox('Do you need parking space?',['No','Yes']))
 
 
-# Create the input list 
-input_list = [lt_t,spcl,price_t,adults,wkend,park,wk,mkt,amnth,awkd,totn,dwkd]
+# Transform the data
+lt_t,price_t = transformer.transform([[lt,price]])[0]
 
-#Make predictions
-predictions = model.predict_proba([input_list])[:,1][0]
+# Create input list
+input_list = [lt_t,spcl,price_t,adults,wkend,park,wk,mrkt,amnth,awkd,totn,dwkd]
 
-#lets show the probabality 
+# Predict the outcome
+
+prediction = model.predict_proba([input_list])[:,1][0]
+
+# Lets show the probability
 if st.button('Predict'):
-    st.success(f'Cancellation Chances: {round(predictions,4)*100}%')
-    
+    st.success(f'The chances of booking cancellation is : {np.round(prediction*100,2)}%')
+    if prediction>=0.3:
+        st.warning('There are high chances of booking cancellation.Please reconfirm your bookingwith the hotel.')
